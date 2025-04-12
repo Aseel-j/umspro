67eebca1ba2ba54b8d7916372aff2920dbd0fdda
Hadeel-Jumaa <hadeelshjumaa.119@gmail.com>
2025-04-13 00:42:10 +0300

    all

--- src/index.router.js
+++ src/index.router.js
@@ -2,8 +2,10 @@ import {connectDB} from '../DB/connection.js';
 import userRouter from './modules/user/user.router.js';
 import authRouter from './modules/auth/auth.router.js';
 import blogRouter from './modules/blog/blog.router.js';
+import cors from 'cors';
 const initApp =(app,express)=>{
 connectDB();
+app.use(cors());
 app.use(express.json());
 //localhost:3000/users
 app.use('/users',userRouter);
